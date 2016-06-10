ef45cdd92cd1243b413d6285eea041c59bb93215
Benmoony <benmiller92@gmail.com>
2016-06-10 09:42:07 -0700

    Finished lab

--- app/build/generated/source/r/debug/com/example/mycontactlist/R.java
+++ app/build/generated/source/r/debug/com/example/mycontactlist/R.java
@@ -11,12 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int navbar_background=0x7f040000;
-        public static final int toolbar_background=0x7f040001;
+        public static final int dark=0x7f060000;
+        public static final int killer_queen=0x7f060001;
+        public static final int navbar_background=0x7f060002;
+        public static final int toolbar_background=0x7f060003;
+        public static final int truffle=0x7f060004;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int appicon=0x7f020000;
@@ -26,13 +29,13 @@ public final class R {
         public static final int settingsicon=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f09001d;
-        public static final int birthdayPicker=0x7f090019;
+        public static final int action_settings=0x7f09001e;
+        public static final int birthdayPicker=0x7f09001a;
         public static final int btnBirthday=0x7f090017;
-        public static final int btnCancel=0x7f09001b;
-        public static final int btnOk=0x7f09001c;
+        public static final int btnCancel=0x7f09001c;
+        public static final int btnOk=0x7f09001d;
         public static final int buttonSave=0x7f090006;
-        public static final int dateSeletLayout=0x7f090018;
+        public static final int dateSeletLayout=0x7f090019;
         public static final int editAddress=0x7f09000b;
         public static final int editCell=0x7f090011;
         public static final int editCity=0x7f09000c;
@@ -46,7 +49,7 @@ public final class R {
         public static final int imageButtonSettings=0x7f090003;
         public static final int navbar=0x7f090000;
         public static final int scrollView1=0x7f090007;
-        public static final int tableRow1=0x7f09001a;
+        public static final int tableRow1=0x7f09001b;
         public static final int textAddress=0x7f09000a;
         public static final int textBday=0x7f090015;
         public static final int textBirthday=0x7f090016;
@@ -54,6 +57,7 @@ public final class R {
         public static final int textContact=0x7f090008;
         public static final int textEMail=0x7f090014;
         public static final int textHome=0x7f090010;
+        public static final int textView=0x7f090018;
         public static final int toggleButtonEdit=0x7f090005;
         public static final int toolbar=0x7f090004;
     }
@@ -71,25 +75,28 @@ public final class R {
         public static final int contact_settings=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
-        public static final int title_activity_contact_list=0x7f060003;
-        public static final int title_activity_contact_map=0x7f060004;
-        public static final int title_activity_contact_settings=0x7f060005;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int hello_world=0x7f070002;
+        public static final int title_activity_contact_list=0x7f070003;
+        public static final int title_activity_contact_map=0x7f070004;
+        public static final int title_activity_contact_settings=0x7f070005;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f050001;
+        public static final int ColorFont=0x7f050002;
+        public static final int Wrap=0x7f050003;
+        public static final int genLayout=0x7f050004;
     }
 }
